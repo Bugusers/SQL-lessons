218969ab29108293d5f9e3467cb8b4c4a2ed91cd
Raccoon <vsafirko@gmail.com>
2023-12-12 19:47:13 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SQL-lessons
+
+This repo was cteated for my db lessons
---
+++ src/main/java/org/db_lessons/Main.java
@@ -0,0 +1,7 @@
+package org.db_lessons;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
